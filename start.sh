a58d46c6ebc56c7dbcf5a9eda2d9e3ddd873a0f8
SANDMAN <111509527+SANDY-FM@users.noreply.github.com>
2023-01-16 16:26:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devil-Botz/Elsa.git /Elsa
+  git clone https://github.com/SANDY-FM/CALOPY-SANDY.git /CALOPY-SANDY
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /CALOPY-SANDY
 fi
 cd /Elsa
 pip3 install -U -r requirements.txt
