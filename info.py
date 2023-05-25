e67835086745cf3e092f322704612e96f54701a1
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-05-25 14:06:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
 #Verify Features
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 HOW_TO_VERIFY = environ.get("HOW_TO_VERIFY", "https://t.me/HOW_TO_DOWNLOADDS/3")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'e54dda33bf4ce87a13a4e0c7ec42bf46a83f3838')
