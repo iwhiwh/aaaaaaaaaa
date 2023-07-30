3cd24f2329a2625ed87cb26c518d1958aec789c6
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-30 20:25:21 +0530

    Update ping.py

--- plugins/helpers/ping.py
+++ plugins/helpers/ping.py
@@ -23,7 +23,7 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"<b>Pong!\n{time_taken_s:.3f} ms\n\n@FILMY_MEN</b>")
+    await rm.edit(f"<b>Pong!\n{time_taken_s:.3f} ms\n\n@The_Happy_Hour_Hindi</b>")
 
 
 
