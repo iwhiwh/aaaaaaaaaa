21ccd1b60e4ae5980f388e5c39f8270cad6bc291
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-26 16:20:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -290,7 +290,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🇮🇳 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 🇮🇳', url=(MAIN_CHANNEL)) ] ] ),
+        
         protect_content=True if pre == 'filep' else False,
         )
                     
