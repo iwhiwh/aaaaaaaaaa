44bd4dbe4cd344b5e053df746aff0383ab5855b9
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-26 13:34:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
 #Verify Features
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 HOW_TO_VERIFY = environ.get("HOW_TO_VERIFY", "https://t.me/Kvxohcphcp/3")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'cuty.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd3f0c1fc80abd229ada4ba313')
